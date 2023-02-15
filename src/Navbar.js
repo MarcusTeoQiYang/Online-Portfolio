40d3f7bd8fde0eac5d47195a4f1087b80f946e30
MarcusTeoQiYang <Marcus.teo.sg@gmail.com>
2023-02-15 18:10:15 +0800

    Added mobile responsive navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,11 @@
 import {Link} from 'react-router-dom';
+import {Squash as Hamburger} from 'hamburger-react';
+import React, {useState} from 'react';
 
-const Navbar = () => {
+const Navbar = () => {  
+
+    const [burgerStatus, setBurgerStatus] = useState(false)
+
     return ( 
         <nav className="navbar">
             <h3 className="name">
@@ -15,7 +20,28 @@ const Navbar = () => {
                     {/* <li><Link to='/Interest'>Ideas!!</Link></li> */}
 
                     {/* <li><a href="#contactme">Contact me</a></li> */}
+                    <div className="hamburger">
+                        <Hamburger 
+                        distance='lg'
+                        size = {40}
+                        rounded 
+                        color="#FFFDD0"
+                        onToggle={toggle => {
+                            if (toggle) {
+                                document.getElementsByClassName("hamburger-menu")[0].style.display = "flex";
+                            } else {
+                                document.getElementsByClassName("hamburger-menu")[0].style.display = "none";
+                            }
+                        }}
+                        />
+                    </div>
                 </ul>
+                <div className="hamburger-menu">
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/AboutMe'>About</Link></li>
+                    <li><Link to='/Projects'>Projects</Link></li>
+                    <li><Link to='/Experience'>Experience</Link></li>
+                </div>
             </div>
         </nav>
         );
