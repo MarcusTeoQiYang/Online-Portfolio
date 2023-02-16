53ca7badce31e94a274e05e760ecfabf29a0b09a
MarcusTeoQiYang <Marcus.teo.sg@gmail.com>
2023-02-16 10:44:03 +0800

    Major improvement to mobile responsive navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,22 +4,23 @@ import React, {useState} from 'react';
 
 const Navbar = () => {  
 
-    const [navHeight, setNavHeight] = useState('80px')
+    const [navHeight, setNavHeight] = useState('60px')
     const [isOpen, setOpen] = useState(false)
 
     const handleClick = () => {
-            setNavHeight('80px')
+            setNavHeight('60px')
             setOpen(false)
     }
     return ( 
         <nav className="navbar" style={{height: navHeight}}>
+            <div className="wrap">
             <h3 className="name">
                 Marcus Teo
             </h3>
             <div className="links">
                 <ul>
                     <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/AboutMe'>About</Link></li>
+                    <li><Link to='/AboutMe'>About me</Link></li>
                     <li><Link to='/Projects'>Projects</Link></li>
                     <li><Link to='/Experience'>Experience</Link></li>
                     {/* <li><Link to='/Interest'>Ideas!!</Link></li> */}
@@ -35,19 +36,20 @@ const Navbar = () => {
                         toggle = {setOpen}
                         onToggle={toggled => {
                             if (toggled) {
-                                setNavHeight('250px');
+                                setNavHeight('235px');
                             }
                             else {
-                                setNavHeight('80px');
+                                setNavHeight('60px');
                             }
                         }}
                         />
                     </div>
                 </ul>
             </div>
+            </div>
             <div className="hamburger-menu">
                     <li onClick={handleClick}><Link to='/'>Home</Link></li>
-                    <li onClick={handleClick}><Link to='/AboutMe'>About</Link></li>
+                    <li onClick={handleClick}><Link to='/AboutMe'>About me</Link></li>
                     <li onClick={handleClick}><Link to='/Projects'>Projects</Link></li>
                     <li onClick={handleClick}><Link to='/Experience'>Experience</Link></li>
             </div>
